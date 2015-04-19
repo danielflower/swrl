d79aba21f2c2aab690baccbf3c7ffd61ea61a6c4
Daniel Flower <git@danielflower.com>
2015-04-19 16:40:32 +0800

    Enforcing unique email addresses

--- migrations/201504220558-add-users-table.up.sql
+++ migrations/201504220558-add-users-table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users
 (
   id         SERIAL PRIMARY KEY,
   username   VARCHAR(50)  NOT NULL UNIQUE,
-  email      VARCHAR(100) NOT NULL,
+  email      VARCHAR(100) NOT NULL UNIQUE,
   admin      BOOLEAN      NOT NULL,
   last_login TIME         NULL,
   is_active  BOOLEAN      NOT NULL,
