61d10277b68af23baebff8ad938ee1190b139349
Daniel Flower <git@danielflower.com>
2015-04-19 17:31:07 +0800

    Changing the way the DB URL is loaded and made the template a bit more HTML5y

--- src/yswrl/db/core.clj
+++ src/yswrl/db/core.clj
@@ -1,14 +1,22 @@
-(ns yswrl.db.core
-  (:use korma.core
-        [korma.db :only (defdb)]))
+(ns yswrl.db.core)
+(use 'korma.db)
+(use 'korma.core)
 
-(def db-spec
-  {:subprotocol "postgresql"
-   :subname "//localhost/yswrl"
-   :user "dev"
-   :password "password"})
 
-(defdb db db-spec)
+(def db-uri (or (System/getenv "DATABASE_URL")
+                "postgres://dev:password@localhost:5432/yswrl"))
+
+(defn convert-db-uri [db-uri]
+  (let [[_ user password host port db] (re-matches #"postgres://(?:(.+):(.*)@)?([^:]+)(?::(\d+))?/(.+)" db-uri)]
+    {
+     :user     user
+     :password password
+     :host     host
+     :port     (or port 5432)
+     :db       db
+     }))
+
+(defdb db (postgres (convert-db-uri db-uri)))
 
 (defentity users
            (database db)
@@ -16,9 +24,9 @@
 
 (defn create-user [username email password]
   (insert users
-          (values { :username username :email email :password password :admin false :is_active true  } )))
+          (values {:username username :email email :password password :admin false :is_active true})))
 
 (defn get-user [username]
   (first (select users
-                 (where {:username username })
+                 (where {:username username})
                  (limit 1))))
