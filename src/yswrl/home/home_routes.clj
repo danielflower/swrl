3d0e6ff1cdfb783621512c74a15a275c6edf389a
Daniel Flower <github@danielflower.com>
2015-04-28 16:44:17 +1200

    Rearranged the furniture

--- src/yswrl/home/home_routes.clj
+++ src/yswrl/home/home_routes.clj
@@ -4,11 +4,8 @@
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render "home.html"))
+  (layout/render "home/home.html"))
 
-(defn about-page []
-  (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page)))
