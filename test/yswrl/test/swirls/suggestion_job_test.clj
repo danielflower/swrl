d6ae008de29db7078301c24da76711b39a35f673
Daniel Flower <github@danielflower.com>
2015-05-09 22:41:31 +1200

    Fixed suggestion emails test

--- test/yswrl/test/swirls/suggestion_job_test.clj
+++ test/yswrl/test/swirls/suggestion_job_test.clj
@@ -1,11 +1,13 @@
 (ns yswrl.test.swirls.suggestion-job-test
   (:use clojure.test)
-  (:require [yswrl.swirls.suggestion-job :as sug]))
+  (:require [yswrl.swirls.suggestion-job :as sug])
+  (:import (java.util UUID)))
 
 (deftest comment-notifier
   (testing "an HTML template is created with links to the swirl"
-    (let [html (sug/suggestion-email-html {:swirl_id 1 :title "Some <great> thing" :author_name "F<rank>" :code "AFAJKDFH"})]
-      (is (.contains html (str "<a href=\"http://www.youshouldwatchreadlisten.com/swirls/1?code=AFAJKDFH\">Some &lt;great&gt; thing</a>")) html)
+    (let [code (UUID/randomUUID)
+          html (sug/suggestion-email-html {:swirl_id 1 :title "Some <great> thing" :author_name "F<rank>" :code code})]
+      (is (.contains html (str "<a href=\"http://www.youshouldwatchreadlisten.com/swirls/1?code=" (str code) "\">Some &lt;great&gt; thing</a>")) html)
       (is (.contains html (str "F&lt;rank&gt; has recommended that you check out")) html))))
 
 
