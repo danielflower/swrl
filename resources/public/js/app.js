f81b51a895dc3a9f35c7ebf74db5574482eacf3e
Daniel Flower <git@danielflower.com>
2015-05-23 22:57:50 +0800

    Tweaking chrome installation

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -39,6 +39,7 @@ $(document).ready(function () {
         $('.chrome-only').css('display', 'block');
 
         if (chrome.app.isInstalled) {
+            // Note: this always returns false even when installed
             $('.chrome-extension-installed').css('display', 'block');
         } else {
             $('.install-chrome-extension-box').css('display', 'block');
@@ -47,6 +48,7 @@ $(document).ready(function () {
                 chrome.webstore.install(undefined,
                     function (suc) {
                         $('.chrome-extension-installed').css('display', 'block');
+                        $('.install-chrome-extension-box').css('display', 'none');
                         console.log('Installation succeeded', suc);
                     },
                     function (err) {
