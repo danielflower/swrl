4c268e15e29ab18cf059473197984c8823d84e91
Daniel Flower <git@danielflower.com>
2015-05-24 13:25:58 +0800

    Now requests to old URLs or the heroku URL will redirect with the canonical domain for Swirl

--- src/yswrl/middleware.clj
+++ src/yswrl/middleware.clj
@@ -10,9 +10,15 @@
             [ring.middleware.format :refer [wrap-restful-format]]
             [buddy.auth.middleware :refer [wrap-authentication]]
             [buddy.auth.backends.session :refer [session-backend]]
+            [yswrl.links :as linky]
             ))
 
-
+(defn permanent-redirect
+  "Returns a Ring response for an HTTP 301 redirect."
+  [url]
+  {:status  301
+   :headers {"Location" url}
+   :body    ""})
 
 (defn log-request [handler]
   (fn [req]
@@ -28,16 +34,27 @@
 
 (def unsecure-key-for-dev-mode "93762d738951e53a")          ; in heroku, a value similar to this is stored in an env var. It just needs to be any non-changing value
 
+(def allowed-hosts #{"www.swrl.co"})
+
+(defn wrap-url-canonicalizer-policy [handler]
+  (fn [req]
+    (let [host (get (req :headers) "host")]
+      (if (or (contains? allowed-hosts host) (.startsWith host "localhost"))
+        (handler req)
+        (permanent-redirect (str (linky/absolute (req :uri))))))))
+
+
 (defn get-unhandled-error-text [ex]
-  (let [stack-strings (map #(str %) (.getStackTrace ex)) ]
-  (str "Unhandled exception: " ex \newline " at " (clojure.string/join (str \newline "    ") stack-strings))))
+  (let [stack-strings (map #(str %) (.getStackTrace ex))]
+    (str "Unhandled exception: " ex \newline " at " (clojure.string/join (str \newline "    ") stack-strings))))
 
 (defn production-middleware [handler]
   (-> handler
+      (wrap-url-canonicalizer-policy)
       (wrap-authentication (session-backend))
       (wrap-restful-format :formats [:json-kw :edn :transit-json :transit-msgpack])
       (wrap-defaults
         (-> site-defaults
-        (assoc-in [:session :store] (cookie-store {:key (or (System/getenv "SECRET_COOKIE_KEY") unsecure-key-for-dev-mode)}))
-        (assoc-in [:session :cookie-name] "yswrl-session")))
+            (assoc-in [:session :store] (cookie-store {:key (or (System/getenv "SECRET_COOKIE_KEY") unsecure-key-for-dev-mode)}))
+            (assoc-in [:session :cookie-name] "yswrl-session")))
       (wrap-internal-error :log #(log/error (get-unhandled-error-text %)))))
