2088c9cdb5aa20f2409ef1f351ed29027f911c59
Daniel Flower <git@danielflower.com>
2015-07-05 19:11:52 +0800

    Added not-logged-in and logged-in homepages

--- src/yswrl/home/home_routes.clj
+++ src/yswrl/home/home_routes.clj
@@ -1,20 +1,25 @@
 (ns yswrl.home.home-routes
   (:require [yswrl.layout :as layout]
             [yswrl.links :as linky]
-            [compojure.core :refer [defroutes GET]]))
+            [compojure.core :refer [defroutes GET]]
+            [yswrl.swirls.lookups :as lookups]))
 
 
 
-(defn home-page []
-  (layout/render "home/home.html"))
+(defn home-page [user]
+  (if (nil? user)
+    (layout/render "home/home-not-logged-in.html")
+    (layout/render "home/home-logged-in.html" {:swirls (lookups/get-swirls-awaiting-response (:id user) 100 0)})))
 
 (defn bookmarklet []
-  (str "javascript:(function(){location.href='" (linky/url-encode (linky/absolute "/create/from-url?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);}());")) ))
+  (str "javascript:(function(){location.href='" (linky/url-encode (linky/absolute "/create/from-url?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);}());"))))
 
 (defn bookmarklet-page []
-  (layout/render "home/bookmarklet.html" { :bookmarklet (bookmarklet) }))
+  (layout/render "home/bookmarklet.html" {:bookmarklet (bookmarklet)}))
 
+
+(defn session-from [req] (:user (:session req)))
 
 (defroutes home-routes
-           (GET "/" [] (home-page))
+           (GET "/" [:as req] (home-page (session-from req)))
            (GET "/how-to-add" [] (bookmarklet-page)))
