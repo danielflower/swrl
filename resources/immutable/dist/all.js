516f2e98d9d7c25997976b0ef22e5680f7ae7f00
Daniel Flower <git@danielflower.com>
2015-07-06 22:09:57 +0800

    Fiddled with the HTML and CSS a bit so that the tests pass. Specifically, had to change some h2 tags to h1 (I made them look like h2s using the power of CSS) and had to break the mini-swirl HTML have two links - one with just the swirl title - such that the test could find the link to click on it. The click target remains the same as before.

--- resources/immutable/dist/all.js
+++ resources/immutable/dist/all.js
@@ -47,7 +47,7 @@
     return function () {
       setTimeout(lib$es6$promise$asap$$flush, 1);
     };
-  }var lib$es6$promise$asap$$queue = new Array(1e3);function lib$es6$promise$asap$$flush() {
+  }var lib$es6$promise$asap$$queue = new Array(1000);function lib$es6$promise$asap$$flush() {
     for (var i = 0; i < lib$es6$promise$asap$$len; i += 2) {
       var callback = lib$es6$promise$asap$$queue[i];var arg = lib$es6$promise$asap$$queue[i + 1];callback(arg);lib$es6$promise$asap$$queue[i] = undefined;lib$es6$promise$asap$$queue[i + 1] = undefined;
     }lib$es6$promise$asap$$len = 0;
