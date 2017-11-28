aa0f5437edc20b78acad2b8cab1894af495b1944
Kyle Harrison <kylejharrison@gmail.com>
2017-11-28 15:56:01 +0000

    add route to get user avatar

--- src/yswrl/rest/user_resource.clj
+++ src/yswrl/rest/user_resource.clj
@@ -3,8 +3,16 @@
     [ring.middleware.params :refer [wrap-params]]
     [ring.util.response :refer [status redirect response not-found]]
     [compojure.core :refer [defroutes context GET POST]]
-    [yswrl.auth.auth-repo :as user-repo]))
+    [yswrl.auth.auth-repo :as user-repo]
+    [yswrl.rest.utils :as utils]
+    [clojure.tools.logging :as log]))
 
 (defroutes user-resource-rest-routes
            (GET "/api/v1/users" []
-             (response (user-repo/users-for-dropdown))))
+             (response (user-repo/users-for-dropdown)))
+
+           (GET "/api/v1/user-avatar" [user_id]
+             (try (utils/json-response (user-repo/get-avatar-link-from-user-id (Integer/parseInt user_id) 200))
+                  (catch Exception e
+                    (log/info e "Failed to get user avatar for user_id: " user_id)
+                    (utils/json-response {:message (str "Failed to get user avatar for user_id: " user_id)} 500)))))
